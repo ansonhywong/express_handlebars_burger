21e2e2b7203f118558ce5051f2da978148be06dc
Anson Wong <ansonhywong@gmail.com>
2020-07-09 02:13:10 -0400

    update

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ("The Beast Burger");
+INSERT INTO burgers (burger_name) VALUES ("If you dare Cheeseburger");
+INSERT INTO burgers (burger_name) VALUES ("Try me Burger");
